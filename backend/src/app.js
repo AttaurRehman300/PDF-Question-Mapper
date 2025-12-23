feea6d12787e487eff6b0c0fcc2830fbf350b9d6
Atta ur Rehman <163998748+AttaurRehman300@users.noreply.github.com>
2025-12-23 18:56:54 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -104,7 +104,7 @@ module.exports = app;
 //       .catch(err => console.log('⚠️ MongoDB not connected'));
 //   }
   
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-  });
-}
+//   app.listen(PORT, () => {
+//     console.log(`🚀 Server running on http://localhost:${PORT}`);
+//   });
+// }
